0c17b817eb90dde90983ff9d3167d6b0643d9e9a
Yulia Nedyalkova <yulia.nedyalkova@sap.com>
2016-08-05 17:00:42 +0300

    Add concourse tests
    Signed-off-by: Yulia Nedyalkova <yulia.nedyalkova@sap.com>

---
+++ concourse/ginkgo-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p $GOPATH/src
+
+echo "Moving project to GOPATH..."
+prefix_path=$GOPATH/src/github.infra.hana.ondemand.com/cloudfoundry
+mkdir -p $prefix_path
+cp -r aker-oauth-plugin $prefix_path
+cd $prefix_path/aker-oauth-plugin
+
+echo "Fetching test tools..."
+go get github.com/onsi/ginkgo/ginkgo
+
+echo "Running tests..."
+
+# setting the timezone is necessary in order
+# to get the correct token expirity date
+export TZ='Europe/Sofia'
+
+ginkgo -r
